e9845e3b5df5c199f7c9b0dd9950248552fc23bb
RoeunSaoly <saolyroeun@gmail.com>
2025-07-20 14:31:58 +0700

    Your commit message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// Home page
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is home page</h1>
   </main>
 </template>
