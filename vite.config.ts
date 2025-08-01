830ff8c7b9eccc2ba661d0810090aaa5a5afff25
Mai Reaksa <mai.reaksa@sabay.com>
2025-08-01 09:17:17 +0700

    add config docker file

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    host: '0.0.0.0'
+  },
   plugins: [
+
     vue(),
     vueDevTools(),
     tailwindcss(),
